299234d62688e449b19ef027b0453ff87ce9e675
Bleighhhh <davidefmaxage@gmail.com>
2022-08-18 16:49:37 +0200

    modifica

--- trade.go
+++ trade.go
@@ -1,1 +1,14 @@
 package trade
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+func registerTrade() {
+	
+}
+
+https://medium.com/kanoteknologi/better-way-to-read-and-write-json-file-in-golang-9d575b7254f2
+
+https://www.developer.com/languages/json-files-golang/
